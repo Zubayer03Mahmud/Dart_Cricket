33d88270322908a7493c76ba0a136da196e5075b
Zubayer03Mahmud <zubayer.mahmud03@gmail.com>
2026-04-25 20:06:58 +0600

    vercel deployment issue

--- server.js
+++ server.js
@@ -3,17 +3,23 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const authRoutes = require('./auth');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname));
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
 
 // Routes
 app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/auth', (req, res) => {
   res.sendFile(path.join(__dirname, 'auth.html'));
 });
 
